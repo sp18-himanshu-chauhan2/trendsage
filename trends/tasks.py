b03ed6ffc03800bda2aa8a59deaa3db00112b315
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-08 15:57:17 +0530

    added new task: process_trend_query

--- trends/tasks.py
+++ trends/tasks.py
@@ -6,14 +6,15 @@ from .services import fetch_trends_mock
 
 logger = logging.getLogger(__name__)
 
+
 @shared_task
-def test_celery():
+def test_celery():  # Simple test task to verify Celery is working
     logger.info("Celery task executed successfully.")
     return "Hello from Celery!"
 
 
 @shared_task
-def fetch_trends_mock_task():
+def fetch_trends_mock_task():  # Task to fetch mock trends for testing
     q = TrendQuery.objects.create(
         industry='EdTech',
         region='India',
@@ -23,3 +24,17 @@ def fetch_trends_mock_task():
     fetch_trends_mock(q)
     logger.info(f"Mock trends fetched for query ID {q.id}")
     return f"Mock trends fetched for query ID {q.id}"
+
+
+@shared_task
+def process_trend_query(query_id):
+    try:
+        query = TrendQuery.objects.get(id=query_id)
+        fetch_trends_mock(query)  # replace with real service later
+        query.status = 'completed'
+        query.save()
+        logger.info(f"Trends fetched for query {query.id}")
+        return f"Trends fetched for query {query.id}"
+    except TrendQuery.DoesNotExist:
+        logger.error(f"TrendQuery {query.id} not found")
+        return f"TrendQuery {query.id} not found"

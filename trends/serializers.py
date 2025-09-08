cc48cbc333788a38b9af6d7a9ff6069331e85439
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-08 15:54:00 +0530

    created serializers

---
+++ trends/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from .models import TrendQuery, TrendResult
+
+
+class TrendResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrendResult
+        fields = [
+            'id',
+            'topic',
+            'summary',
+            'sources',
+            'final_score',
+            'suggested_angles',
+            'created_at',
+        ]
+
+
+class TrendQuerySerializer(serializers.ModelSerializer):
+    results = TrendResultSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = TrendQuery
+        fields = [
+            'id',
+            'industry',
+            'region',
+            'persona',
+            'date_range',
+            'status',
+            'created_at',
+            'results',
+        ]
+
+
+class TrendQueryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrendQuery
+        fields = [
+            'id',
+            'industry',
+            'region',
+            'persona',
+            'date_range',
+        ]

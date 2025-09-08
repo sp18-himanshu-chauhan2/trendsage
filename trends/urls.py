fdb58389d27de85edb1cadf9a5260e1361325db6
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-08 16:00:55 +0530

    created urls

---
+++ trends/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import TrendListView, TrendQueryDetailView, TrendQueryCreateView
+
+
+urlpatterns = [
+    path('trends/', TrendListView.as_view(), name='trend-list'),
+    path('trends/query/<uuid:id>/', TrendQueryDetailView.as_view(),
+        name='trend-query-detail'),
+    path('trends/query/', TrendQueryCreateView.as_view(),
+        name='trend-query-create'),
+]

4c77a80a09e9782331844431efc72be338cd07b2
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-09 16:01:29 +0530

    fetched trends from perplexity instead of mock trends

--- trends/tasks.py
+++ trends/tasks.py
@@ -1,40 +1,31 @@
+from .services import fetch_trends_from_perplexity
 from celery import shared_task
-import logging
 from .models import TrendQuery
-from .services import fetch_trends_mock
-
+import logging
 
 logger = logging.getLogger(__name__)
 
 
-@shared_task
-def test_celery():  # Simple test task to verify Celery is working
-    logger.info("Celery task executed successfully.")
-    return "Hello from Celery!"
-
-
-@shared_task
-def fetch_trends_mock_task():  # Task to fetch mock trends for testing
-    q = TrendQuery.objects.create(
-        industry='EdTech',
-        region='India',
-        persona='founders',
-        date_range='last_week',
-    )
-    fetch_trends_mock(q)
-    logger.info(f"Mock trends fetched for query ID {q.id}")
-    return f"Mock trends fetched for query ID {q.id}"
-
-
-@shared_task
-def process_trend_query(query_id):
+@shared_task(bind=True)
+def process_trend_query(self, query_id):
     try:
         query = TrendQuery.objects.get(id=query_id)
-        fetch_trends_mock(query)  # replace with real service later
-        query.status = 'completed'
+        query.status = 'running'
         query.save()
+
+        fetch_trends_from_perplexity(query)
+
         logger.info(f"Trends fetched for query {query.id}")
         return f"Trends fetched for query {query.id}"
+
     except TrendQuery.DoesNotExist:
         logger.error(f"TrendQuery {query.id} not found")
-        return f"TrendQuery {query.id} not found"
+        return self.retry(countdown=10, max_retries=3)
+
+    except Exception as e:
+        query = TrendQuery.objects.filter(id=query_id).first()
+        if query:
+            query.status = 'failed'
+            query.save()
+        logger.error(f"Error processing query {query_id}: {e}")
+        raise e

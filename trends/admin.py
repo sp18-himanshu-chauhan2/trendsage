c8a09c4a4460e4c757aef22bcb9d11c51946697c
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-15 11:37:29 +0530

    added user model

--- trends/admin.py
+++ trends/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TrendQuery, TrendResult
+from .models import TrendQuery, TrendResult, User
 
 # Register your models here.
 admin.site.register(TrendQuery)
 admin.site.register(TrendResult)
+admin.site.register(User)
--- trends/models.py
+++ trends/models.py
@@ -1,5 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
 import uuid
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = None
+    email = models.EmailField(unique=True)
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class TrendQuery(models.Model):
@@ -10,6 +45,9 @@ class TrendQuery(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE, related_name="trend_queries",             
+            null=True, blank=True)
     industry = models.CharField(max_length=100)
     region = models.CharField(max_length=100)
     persona = models.CharField(max_length=100)

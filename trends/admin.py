48551016d015df74a2beead9e6bda6f621ac143f
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-18 15:43:35 +0530

    registered subscription model on admin

--- trends/admin.py
+++ trends/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import TrendQuery, TrendResult, User
+from .models import TrendQuery, TrendResult, User, QuerySubscription
 
 # Register your models here.
 admin.site.register(TrendQuery)
 admin.site.register(TrendResult)
 admin.site.register(User)
+
+@admin.register(QuerySubscription)
+class QuerySubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "query", "wants_emails", "created_at")
+    list_filter = ("wants_emails",)
+    search_fields = ("user__email", "query__industry", "query__region")

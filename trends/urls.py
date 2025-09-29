46a6f6d7ca63f4d54c1cc373dfee4d38dcd0fb78
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-09-29 16:36:08 +0530

    updated urls

--- trends/urls.py
+++ trends/urls.py
@@ -4,7 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    # path('trends/', TrendListView.as_view(), name='trend-list'),
     path('trends/query/<uuid:id>/', TrendQueryDetailView.as_view(),
         name='trend-query-detail'),
     path('trends/query/', TrendQueryCreateView.as_view(),
@@ -13,7 +12,6 @@ urlpatterns = [
         name="trend-result-detail"),
     
     # Auth
-    # path("auth/signup/", SignupAPI.as_view(), name="api-signup"),
     path("auth/signup/start/", SignupStartAPI.as_view(), name="api-signup-start"),
     path("auth/signup/verify/", SignupVerifyAPI.as_view(), name="api-signup-verify"),
     path("auth/login/", LoginAPI.as_view(), name="api-login"),
@@ -26,7 +24,7 @@ urlpatterns = [
     path("trends/query/<uuid:query_id>/subscription/", QuerySubscriptionToggleAPI.as_view(), name="api-query-subscription"),
     path("trends/query/<uuid:query_id>/subscription/toggle/", ToggleSubscriptionAPI.as_view(), name="toggle-subscription-api"),
 
-    #Profile
+    # Profile
     path("auth/token/", obtain_auth_token, name="api_token_auth"),
     path("profile/me/", MeAPIView.as_view(), name="api-me"),
 ]
